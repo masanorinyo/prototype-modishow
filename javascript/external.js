d4bac4cebb65f747c2b905787a7a315ae23d9206
masanorinyo <masanorinyo@gmail.com>
2013-11-20 16:16:34 -0500

    Index page - marquee slideshow is added

--- javascript/external.js
+++ javascript/external.js
@@ -33,6 +33,210 @@ $(function(){
 
 
 //********************Index html********************//
+//------------------Slide Show--------------------//
+// JavaScript Document
+// Written by Chris Converse
+// for Lynda.com
+
+$(document).ready(function(){
+
+	// Generate Navigation links
+	$('.marquee_panels .marquee_panel').each(function(index){
+		$('.marquee_nav').append('<a class="marquee_nav_item" ></a>');
+	});
+	
+	// Generate Photo Lineup
+	var k=0;
+	$('img.marquee_panel_photo').each(function(index){
+		console.log("num of loop"+" = " + k++);
+		var photoWidth = $('.marquee_container').width();
+		console.log("width of photo" +" = "+ photoWidth);
+		var photoPosition = index * photoWidth;
+		console.log("photo position is" + " = " + photoPosition);
+		$('.marquee_photos').append('<img class="marquee_photo" style="left: '+photoPosition+'; float:left" src="'+$(this).attr('src')+'" alt="'+$(this).attr('alt')+'" width='+photoWidth+' height="100%" />');
+		$('.marquee_photos').css('width', photoPosition+photoWidth);
+	});
+
+	// Set up Navigation Links
+	$('.marquee_nav a.marquee_nav_item').click(function(){
+		
+		// Set the navigation state
+		$('.marquee_nav a.marquee_nav_item').removeClass('selected');
+		$(this).addClass('selected');
+		
+		var navClicked = $(this).index();
+		var marqueeWidth = $('.marquee_container').width();
+		var distanceToMove = marqueeWidth*(-1);
+		var newPhotoPosition = navClicked*distanceToMove + 'px';
+		var newCaption = $('.marquee_panel_caption').get(navClicked);
+		
+		// Animate the photos and caption
+		$('.marquee_photos').animate({left: newPhotoPosition}, 1000);
+		$('.marquee_caption').animate({top: '440px'}, 500, function(){
+			var newHTML = $(newCaption).html();
+			$('.marquee_caption_content').html(newHTML);
+			setCaption();
+		});
+	});
+	
+	// Preload all images, then initialize marquee
+	$('.marquee_panels img').imgpreload(function(){
+		initializeMarquee();
+	});
+
+});
+
+function initializeMarquee(){
+	$('.marquee_caption_content').html(
+		$('.marquee_panels .marquee_panel:first .marquee_panel_caption').html()
+	);
+	$('.marquee_nav a.marquee_nav_item:first').addClass('selected');
+	$('.marquee_photos').fadeIn(1500);
+	setCaption();
+}
+
+function setCaption(){
+	var captionHeight = $('.marquee_caption').height();
+	var marqueeHeight = $('.marquee_container').height();
+	var newCaptionTop = marqueeHeight - captionHeight - 15;
+	$('.marquee_caption').delay(100).animate({top: newCaptionTop}, 500);
+}
+
+
+function imgpreload(imgs,settings)
+{
+	// settings = { each:Function, all:Function }
+	if (settings instanceof Function) { settings = {all:settings}; }
+
+	// use of typeof required
+	// https://developer.mozilla.org/En/Core_JavaScript_1.5_Reference/Operators/Special_Operators/Instanceof_Operator#Description
+	if (typeof imgs == "string") { imgs = [imgs]; }
+
+	var loaded = [];
+	var t = imgs.length;
+	var i = 0;
+
+	for (i; i<t; i++)
+	{
+		var img = new Image();
+		img.onload = function()
+		{
+			loaded.push(this);
+			if (settings.each instanceof Function) { settings.each.call(this); }
+			if (loaded.length>=t && settings.all instanceof Function) { settings.all.call(loaded); }
+		};
+		img.src = imgs[i];
+
+		console.log(i+" = "+img.src);
+	}
+}
+
+if (typeof jQuery != "undefined")
+{
+	(function($){
+
+		// extend jquery (because i love jQuery)
+		$.imgpreload = imgpreload;
+
+		// public
+		$.fn.imgpreload = function(settings)
+		{
+			settings = $.extend({},$.fn.imgpreload.defaults,(settings instanceof Function)?{all:settings}:settings);
+
+			this.each(function()
+			{
+				var elem = this;
+
+				imgpreload($(this).attr('src'),function()
+				{
+					if (settings.each instanceof Function) { settings.each.call(elem); }
+				});
+			});
+
+			// declare urls and loop here (loop a second time) to prevent
+			// pollution of above closure with unnecessary variables
+
+			var urls = [];
+
+			this.each(function()
+			{
+				urls.push($(this).attr('src'));
+			});
+
+			var selection = this;
+
+			imgpreload(urls,function()
+			{
+				if (settings.all instanceof Function) { settings.all.call(selection); }
+			});
+
+			return this;
+		};
+
+		// public
+		$.fn.imgpreload.defaults =
+		{
+			each: null // callback invoked when each image in a group loads
+			, all: null // callback invoked when when the entire group of images has loaded
+		};
+
+	})(jQuery);
+}
+
+/*
+
+	Usage:
+
+	$('#content img').imgpreload(function()
+	{
+		// this = jQuery image object selection
+		// callback executes when all images are loaded
+	});
+
+	$('#content img').imgpreload
+	({
+		each: function()
+		{
+			// this = dom image object
+			// callback executes when each image loads
+		},
+		all: function()
+		{
+			// this = jQuery image object selection
+			// callback executes when all images are loaded
+		}
+	});
+
+	$.imgpreload('/images/a.gif',function()
+	{
+		// this = new image object
+		// callback
+	});
+
+	$.imgpreload(['/images/a.gif','/images/b.gif'],function()
+	{
+		// this = array of new image objects
+		// callback executes when all images are loaded
+	});
+
+	$.imgpreload(['/images/a.gif','/images/b.gif'],
+	{
+		each: function()
+		{
+			// this = new image object
+			// callback executes on every image load
+		},
+		all: function()
+		{
+			// this = array of new image objects
+			// callback executes when all images are loaded
+		}
+	});
+
+*/
+
+
+
 //---------------------Modal box---------------------//
 //--Open and close modal box--//
 $(function(){
@@ -603,8 +807,8 @@ $(function(){
 		var itemsArray = $("#outfitItems > li");
 		//make the items draggable
 	 	$( ".draggable > li" ).draggable({
-	 		
 	 		opacity: 0.5,
+
 	 	});
 		
 		$(".draggable > li").mousedown(function(event){
@@ -808,28 +1012,6 @@ $(function(){
 	});
 });
 
-$(function($){
-	var scrolling = false;
-
-  	
-
-	function startScrolling(obj, param){
-		console.log(obj);
-	    $(obj).animate({"left":"param"}, "fast", function(){
-	    	console.log(param+"px");
-	    	if (scrolling){
-	    		startScrolling(obj, param);
-	    	};
-	    });
-	};
-
-	$(".rightArrow_subSlideshow_two").mousedown(function(){
-    	scrolling = true;
-    	startScrolling($(".slider-middle > li"), "+=10");
-    }).mouseup(function(){
-    	scrolling = false;
-    });
-});
 
 //start from here
 $(function(){
